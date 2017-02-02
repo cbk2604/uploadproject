828777dfe73ef5485a70f7bffedea41e45c6951f
Bharghav <bharghav@happay.in>
2017-02-02 16:23:19 +0530

    FIRST COMMIT

---
+++ UPLOADTOGITHUB/UPLOADTOGITHUB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UPLOADTOGITHUB
+//
+//  Created by Happay on 02/02/17.
+//  Copyright © 2017 SSS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
